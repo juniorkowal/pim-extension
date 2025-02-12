646e166627fa49c2599aa39551082d4a29ff9278
juniorkowal <juniorkowal123@gmail.com>
2025-02-12 22:17:40 +0100

    Delete external lib

--- setup.py
+++ setup.py
@@ -16,10 +16,6 @@ setup(
                 'src/ops/add.cpp',
                 'src/ops/transpose.cpp'
             ],
-            include_dirs=[os.path.join(PIMBLAS_PATH, "include")],
-            library_dirs=[os.path.join(PIMBLAS_PATH, "lib")],
-            libraries=["pimblas"],
-            extra_compile_args=['-std=c++14'],
         ),
     ],
     cmdclass={'build_ext': BuildExtension},
