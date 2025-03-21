d298e7482465caac4c7afa8565aa2196aca998e1
juniorkowal <122057361+juniorkowal@users.noreply.github.com>
2025-03-21 13:45:23 +0100

    Delete commented code from extension.cpp

--- src/extension.cpp
+++ src/extension.cpp
@@ -1,24 +1,11 @@
 #include <torch/script.h>
 #include <torch/extension.h>
 #include "logging.h"
-//#include <spdlog/spdlog.h>
 
 
 torch::Tensor mm(const at::Tensor& a, const at::Tensor& b);
 torch::Tensor add(const at::Tensor& a, const at::Tensor& b);
 torch::Tensor relu(const at::Tensor& a);
-
-
-//void initialize_extension() {
-//    initialize_logging();
-//    spdlog::info("Extension initialized with logging.");
-//}
-
-
-//void initialize_extension() {
-//    initialize_logging();
-//    spdlog::info("Extension initialized with logging.");
-//}
 
 
 TORCH_LIBRARY(hpim, m) {
@@ -33,8 +20,3 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("add", &add, "PIM add implementation");
     m.def("relu", &relu, "PIM relu implementation");
 }
-
-//static auto _ = []() {
-//    initialize_extension();
-//    return 0;
-//}();
