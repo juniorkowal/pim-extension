cd8a166008b9ef3d5d24904c85ac0f480ee1e35e
juniorkowal <juniorkowal123@gmail.com>
2025-04-08 11:29:17 +0200

    Delete 'layers' arg from optimize func

--- src/hpim/hpim_optimize.py
+++ src/hpim/hpim_optimize.py
@@ -134,10 +134,10 @@ from .methods import switch_layers, trace_fx, pim_compile
 #         return switch_layers(model, layers)
 # from .methods import switch_layers, trace_fx, pim_compile
 
-def optimize(model: nn.Module, layers: list = None, mode: str = 'layers'):
+def optimize(model: nn.Module, mode: str = 'layers'):
     assert mode in ['layers', 'fx', 'compile']
     if mode == 'layers':
-        return switch_layers(model, layers)
+        return switch_layers(model)
     elif mode == 'fx':
         return trace_fx(model)
     elif mode == 'compile':
