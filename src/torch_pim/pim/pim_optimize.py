e1c235c9e5008a2697e8fa6f70552e115bf6f99d
juniorkowal <juniorkowal123@gmail.com>
2025-05-19 13:41:37 +0000

    Temporarily deprecate optimize function

--- src/torch_pim/pim/pim_optimize.py
+++ src/torch_pim/pim/pim_optimize.py
@@ -2,7 +2,18 @@ import torch.nn as nn
 from .methods import switch_layers, trace_fx, pim_compile
 
 
+class DeprecationError(Exception):
+    pass
+
 def optimize(model: nn.Module, mode: str = 'layers', *args, **kwargs):
+    raise DeprecationError(
+        "The 'optimize' function is currently depreacted. "
+        "To use the UPMEM extension, please move your input and model to the 'upmem' device "
+        "using .to('upmem') instead.\n\n"
+        "Example usage:\n"
+        "    model = model.to('upmem')\n"
+        "    input = input.to('upmem')\n\n"
+    ) 
     assert mode in ['layers', 'fx', 'compile']
     if mode == 'layers':
         return switch_layers(model)
